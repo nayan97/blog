5f30290dc55aae929abac8bbd0e069d3138312d3
nayan <nayanislam085@gmail.com>
2022-06-16 22:38:36 +0600

    stat again

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
-route::get();
+route::get('admin-dashboard',[DashboardController::class,'showAdminDashboard'])-> name('admin.dashboard');
