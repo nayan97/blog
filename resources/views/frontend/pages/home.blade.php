18d04788283748e3053910b7934388c9f3fe6d04
nayan97 <nayanislam085@gmail.com>
2022-12-06 20:37:31 +0600

    home page slicing done

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -2,4 +2,66 @@
 
 @section('frontend-main')
 
+
+
+    <!-- Home section-->
+    <section id="home">
+      <!-- Video background-->
+      <div id="video-wrapper">
+        <video autoplay="true" loop="true" preload="auto">
+          <source src="frontend/video/video.mp4" type="video/mp4">
+            <source src="frontend/video/video.webm" type="video/webm">
+        </video>
+      </div>
+      <!-- end of video background-->
+      <!-- Home slider-->
+      <div id="home-slider" class="flexslider">
+        <ul class="slides">
+          <li>
+            <div class="slide-wrap">
+              <div class="slide-content bold-text">
+                <div class="container">
+                  <h6>We are fun and young.</h6>
+                  <h1 class="upper smaller">We love design<span class="red-dot"></span></h1>
+                  <p><a href="#" class="btn btn-light-out">Read More</a><a href="#" class="btn btn-color btn-full">Services</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="slide-wrap">
+              <div class="slide-content bold-text">
+                <div class="container">
+                  <h6>Video - Animations - Media</h6>
+                  <h1 class="upper smaller">We make cool things<span class="red-dot"></span></h1>
+                  <p><a href="#" class="btn btn-color">Explore</a><a href="#" class="btn btn-light-out">Join us</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!-- end of home slider-->
+    </section>
+    <!-- end of home section-->
+    
+    
+    @include('frontend.sections.title')
+
+    @include('frontend.sections.expertise')
+
+    @include('frontend.sections.vision')
+    @include('frontend.sections.portfolio')
+    @include('frontend.sections.clients')
+    @include('frontend.sections.testimonial')
+    @include('frontend.sections.blogpart')
+  
+  
+
+ 
+  
+ 
+
 @endsection
